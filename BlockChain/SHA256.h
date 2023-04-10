54b136a6295ef2ebb7ed6df8fe76c58dd4dd352e
thanhdat2306 <121104517+thanhdat2306@users.noreply.github.com>
2023-04-10 20:56:07 +0700

    Update SHA256.h

--- BlockChain/SHA256.h
+++ BlockChain/SHA256.h
@@ -6,5 +6,8 @@ using namespace std;
 class SHA256
 {
 public:
+	template<typename t1, typename t2>
+	auto RIGHTROTAGE(t1 x, t2 y) { return (((x) >> (c)) | ((x) << (32 - (c)))); }
 	static string hash(string input);
+	
 };
