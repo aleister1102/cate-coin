25b5ac3721a53668944b477da3adbc0c6be24bb3
Lê Minh Quân <78531303+aleister1102@users.noreply.github.com>
2023-04-23 21:32:32 +0700

    feat: get balance of node

--- core/blockchain.py
+++ core/blockchain.py
@@ -5,13 +5,13 @@ from utils import compute_hash
 class Block:
     def __init__(self, transactions: list[dict]):
         self.prev_hash = ""
-        self.transactions = json.dumps(transactions)
+        self.transactions = transactions
         self.nonce = 0
         self.hash = ""
         self.total_time = 0
 
     def get_hash(self) -> str:
-        payload = self.prev_hash + self.transactions + str(self.nonce)
+        payload = self.prev_hash + json.dumps(self.transactions) + str(self.nonce)
         return compute_hash(payload)
 
 
@@ -40,6 +40,15 @@ class Blockchain:
         self.find_nonce(block)
         self.chain.append(block)
 
+    def print(self):
+        for block in self.chain:
+            print("Hash: ", block.hash)
+            print("Previous hash:", block.prev_hash)
+            print("Transactions: ", block.transactions)
+            print("Nonce: ", block.nonce)
+            print("Total time: ", block.total_time)
+            print()
+
     def is_valid(self):
         for i in range(1, len(self.chain)):
             current_block = self.chain[i]
@@ -55,24 +64,38 @@ class Blockchain:
                 return False
 
         return True
-
-    def print(self):
+    
+    def get_balance(self, person: str) -> int:
+        balance = 0
         for block in self.chain:
-            print("Previous hash:", block.prev_hash)
-            print("Transactions: ", block.transactions)
-            print("Nonce: ", block.nonce)
-            print("Hash: ", block.hash)
-            print("Total time: ", block.total_time)
-            print()
+            if type(block.transactions) is not list:
+                continue
+            for transaction in block.transactions:
+                if transaction["from"] == person:
+                    balance -= transaction["amount"]
+                if transaction["to"] == person:
+                    balance += transaction["amount"]
+        return balance
+
 
 
 if __name__ == '__main__':
-    transactions = [
+    blockchain = Blockchain()
+    blockchain.add([
         {"from": "Alice", "to": "Bob", "amount": 1},
         {"from": "Bob", "to": "Charlie", "amount": 2},
         {"from": "Charlie", "to": "Dave", "amount": 3},
-    ]
-    blockchain = Blockchain()
-    blockchain.add(transactions)
+    ])
+    blockchain.add([
+        {"from": "Charlie", "to": "Bob", "amount": 5},
+        {"from": "Bob", "to": "Charlie", "amount": 3},
+        {"from": "Charlie", "to": "Alice", "amount": 4},
+    ])
     blockchain.print()
-    print(blockchain.is_valid())
+    
+    print(f'Validity of the blockchain: ', blockchain.is_valid())
+    print()
+
+    print(f'Balance of Alice: {blockchain.get_balance("Alice")}')
+    print(f'Balance of Bob: {blockchain.get_balance("Bob")}')
+    print(f'Balance of Charlie: {blockchain.get_balance("Charlie")}')
