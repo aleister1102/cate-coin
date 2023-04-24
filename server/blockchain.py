a1c18113a0aeea7f033faaef4d208cbfb4e326d4
Lê Minh Quân <78531303+aleister1102@users.noreply.github.com>
2023-04-24 22:24:04 +0700

    feat: new rewarding mechanism - halving reward after sometime - refactor blockchain.py

--- server/blockchain.py
+++ server/blockchain.py
@@ -1,35 +1,37 @@
 import json
 from datetime import datetime
-from core.cryptography import hash, merkle_root
+from cryptography import hash, merkle_root
 
 
 class Transaction:
-    def __init__(self, sender: str, receiver: str, amount: int, change: int = 0) -> None:
-        self.sender: str = sender
-        self.receiver: str = receiver
-        self.amount: int = amount
-        self.change: int = change
-        
+    def __init__(self, sender: str, receiver: str, amount: int, change: int = 0):
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = amount
+        self.change = change
+
     def to_string(self) -> str:
-        return f'{self.sender} -> {self.receiver}: {self.amount}, change: {self.change}'
+        string = f'{self.sender} -> {self.receiver}: {self.amount}, change: {self.change}'
+        return string
+
 
 class Block:
     def __init__(self, transactions: list[Transaction] = []):
         # Body
-        self.transactions: list[Transaction] = transactions
+        self.transactions = transactions
 
         # Header
-        self.previous_hash: str = ""
-        self.merkle_root_hash = self.compute_merkle_root()
-        self.timestamp: str = str(datetime.now())
-        self.target: int = 4
-        self.nonce: int = 0
+        self.previous_hash = ""
+        self.merkle_root_hash = self.compute_merkle_root()  # compute once
+        self.timestamp = str(datetime.now())
+        self.target = 4
+        self.nonce = 0
 
         # Hash
-        self.hash: str = ""
+        self.hash = ""
 
     def to_dict(self) -> dict:
-        return {
+        dictionary = {
             "previous_hash": self.previous_hash,
             "merkle_root_hash": self.merkle_root_hash,
             "timestamp": self.timestamp,
@@ -37,26 +39,35 @@ class Block:
             "hash": self.hash,
             "transactions": [transaction.to_string() for transaction in self.transactions]
         }
-    
-    def to_string(self) -> str:
-        return json.dumps(self.to_dict(), indent=4)
+        return dictionary
+
+    def to_json_string(self) -> str:
+        json_string = json.dumps(self.to_dict(), indent=4)
+        return json_string
 
     def compute_merkle_root(self) -> str:
-        transactions = map(lambda transaction: transaction.to_string(), self.transactions)
+        transactions: list[str] = map(
+            lambda transaction: transaction.to_string(), self.transactions)
         return merkle_root(transactions)
-       
+
     def compute_hash(self) -> str:
-        payload = self.previous_hash + self.merkle_root_hash + self.timestamp + str(self.nonce)
+        payload = f'{self.previous_hash}{self.merkle_root_hash}{self.timestamp}{str(self.nonce)}'
         return hash(payload).hex()
+
 
 class Blockchain:
     def __init__(self):
         self.chain: list[Block] = []
         self.transactions_queue: list[Transaction] = []
-        self.block_capacity: int = 5
+        self.block_capacity = 5
+
+        # Rewarding mechanism
+        self.reward = 128
+        self.interval = 60  # in seconds
+        self.timestamp = datetime.now()
 
         genesis_block = Block()
-        genesis_block.hash = Block.compute_hash(genesis_block)
+        genesis_block.hash = genesis_block.compute_hash()
         self.chain.append(genesis_block)
 
     def add_transaction(self, transaction: Transaction) -> None:
@@ -65,9 +76,18 @@ class Blockchain:
     def add_transactions(self, transactions: list[Transaction]) -> None:
         self.transactions_queue.extend(transactions)
 
-    def get_transactions(self) -> list[Transaction]:
-        return self.transactions_queue
-    
+    def get_current_reward(self) -> int:
+        duration = datetime.now() - self.timestamp
+        seconds = int(duration.total_seconds())
+
+        if seconds != 0:
+            # Halving every interval seconds
+            reward = self.reward / (2 ** int(seconds / self.interval))
+        else:
+            reward = self.reward
+
+        return int(reward)
+
     def get_latest_block(self) -> Block:
         return self.chain[-1]
 
@@ -84,42 +104,42 @@ class Blockchain:
         block.hash = self.proof_of_work(block)
 
         self.chain.append(block)
-     
-    def mine_block(self, miner: str) -> tuple[str, bool]:
+
+    def mine_block(self, miner: str) -> tuple[bool, str]:
         if len(self.transactions_queue) < self.block_capacity - 1:
-            return "Not enough transactions to mine a block", False
+            return False, "Not enough transactions to mine a block"
 
-        transactions: list[Transaction] = []
-        while len(transactions) < self.block_capacity - 1:
+        transactions: list[Transaction] = [
+            Transaction("Owner", miner, self.get_current_reward())]  # rewarding for miner
+        while len(transactions) < self.block_capacity:
             transactions.append(self.transactions_queue.pop(0))
-        transactions.append(Transaction("Owner", miner, 10)) # Reward
-
         self.add_block(transactions)
-        return "Mine successfully", True
+
+        return True, "Mine successfully"
 
     def print(self):
-        for block in self.chain: 
-            print(block.to_string())
+        for block in self.chain:
+            print(block.to_json_string())
             print()
 
-    def is_valid(self) -> tuple[str, bool]:
+    def is_valid(self) -> tuple[bool, str]:
         for i in range(1, len(self.chain)):
             current_block = self.chain[i]
             prev_block = self.chain[i - 1]
 
             if current_block.previous_hash != prev_block.hash:
-                return "Previous hash is not equal to the hash of the previous block", False
+                return False, "Previous hash is not equal to the hash of the previous block"
 
             if current_block.merkle_root_hash != current_block.compute_merkle_root():
-                return "Merkle root hash is not equal to the computed merkle root hash", False
+                return False, "Merkle root hash is not equal to the computed merkle root hash"
 
             if current_block.hash != current_block.compute_hash():
-                return "Hash is not equal to the computed hash", False
+                return False, "Hash is not equal to the computed hash"
 
-            if current_block.hash.startswith(current_block.target * "0") == False:
-                return "Hash does not meet the difficulty target", False
-
-        return "", True
+            if current_block.hash.startswith(current_block.target * "0") is False:
+                return False, "Hash does not meet the difficulty target"
+            
+        return True, None
 
     def get_balance(self, person: str) -> int:
         balance = 0
@@ -139,21 +159,12 @@ if __name__ == '__main__':
     blockchain = Blockchain()
 
     # Add transactions
-    blockchain.add_transactions([
-        Transaction("Alice", "Bob", 40, 60)
-    ])
-    blockchain.add_transactions([
-        Transaction("Eve", "Bob", 60, 55)
-    ])
+    blockchain.add_transaction(Transaction("Alice", "Bob", 40, 60))
+    blockchain.add_transaction(Transaction("Eve", "Bob", 60, 55))
     blockchain.add_transactions([
         Transaction("Alice", "Bob", 100, 30),
         Transaction("Bob", "Charlie", 60, 20),
         Transaction("Charlie", "Alice", 50, 10)
-    ])
-    blockchain.add_transactions([
-        Transaction("Bob", "Alice", 30, 5),
-        Transaction("Alice", "Charlie", 40, 70),
-        Transaction("Charlie", "Bob", 55, 90),
     ])
 
     # Mine blocks
@@ -163,9 +174,5 @@ if __name__ == '__main__':
     # Print blockchain
     blockchain.print()
 
-    # Tamper the blockchain
-    blockchain.chain[1].transactions[0].amount = 1000
-    blockchain.print()
-    
     # Check validity
-    print(f'Validity of the blockchain: ', blockchain.is_valid()[1])
+    print(f'Validity of the blockchain: ', blockchain.is_valid())
--- server/node.py
+++ server/node.py
@@ -1,15 +1,17 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-from core.blockchain import Blockchain, Transaction
+from blockchain import Blockchain, Transaction
 
 app = Flask(__name__)
 blockchain = Blockchain()
+
 
 @app.route("/")
 def home_page():
     # Write API document here
     return "This is the homepage of the API"
+
 
 @app.route('/get-chain', methods=['GET'])
 def handle_get_chain():
@@ -20,35 +22,37 @@ def handle_get_chain():
     }
     return jsonify(response), 200
 
+
 @app.route('/add-transactions', methods=['POST'])
 def handle_add_transactions():
     transactions: list[dict] = request.json['transactions'] if 'transactions' in request.json else []
     transaction_keys = ["sender", "receiver", "amount", "change"]
-    
-    if len(transactions)  == 0:
+
+    if len(transactions) == 0:
         return "No transactions found", 400
 
     for transaction in transactions:
         if not all(key in transaction for key in transaction_keys):
             return f"Some elements of the transaction {transaction} are missing", 400
-        
-        sender, receiver, amount, change = transaction['sender'], transaction['receiver'], transaction['amount'], transaction['change']
-        blockchain.add_transaction(Transaction(sender, receiver, amount, change))
+
+        sender, receiver, amount, change = transaction['sender'], transaction[
+            'receiver'], transaction['amount'], transaction['change']
+        blockchain.add_transaction(Transaction(
+            sender, receiver, amount, change))
 
     response = {
         "message": f"{len(transactions)} transactions will be added to the blockchain soon"
     }
     return jsonify(response), 201
 
+
 @app.route('/mine-block', methods=['GET'])
 def handle_mine_block():
     miner = request.remote_addr
-    mining_message, mining_result = blockchain.mine_block(miner)
+    mining_result, mining_message = blockchain.mine_block(miner)
     latest_block = blockchain.get_latest_block()
 
-    response = {
-        "message": mining_message,
-    }
+    response = {"message": mining_message}
 
     if mining_result is True:
         response["block"] = latest_block.to_dict()
@@ -56,19 +60,21 @@ def handle_mine_block():
     else:
         return jsonify(response), 400
 
+
 @app.route('/is-valid', methods=['GET'])
 def handle_is_valid():
-    error, validity = blockchain.is_valid()
+    validity, error = blockchain.is_valid()
+
     response = {
-        "message": "The blockchain is valid" if validity else "The blockchain is not valid",
+        "message": "The blockchain is valid" if validity else "The blockchain is not valid"
     }
 
-    if error == '':
-        return jsonify(response), 200   
+    if error is None:
+        return jsonify(response), 200
     else:
         response['error'] = error
         return jsonify(response), 400
-        
+
 
 if __name__ == '__main__':
     CORS(app)
